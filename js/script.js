44b7e1ce08cfd654555d4435d2fdf4de88ccf787
Mzulliev <mzulliev@gmail.com>
2023-02-11 18:20:22 +0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,15 @@
+// $(function() {
+// 	$('.menu-btn').click(function(){
+// 		$('header ul').toggleClass('active');
+// 		$('#main .lenguages').toggleClass('leng-actv');
+// 		$(this).toggleClass('menu-btn-active');
+// 	});
+// }); 
+
+// $(function() {
+// 	$('nav li a').click(function() {
+// 		$('header ul').removeClass('active');
+// 		$('#main .lenguages').removeClass('leng-actv');
+// 		$('.menu-btn').removeClass('menu-btn-active');
+// 	})
+// });
